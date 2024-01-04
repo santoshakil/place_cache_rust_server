1b8c20db326946591b30118ac1732b2620dcaad0
Santo Shakil <shakil.mezbah@gmail.com>
2024-01-04 19:14:05 +0600

    ignore

--- src/db.rs
+++ src/db.rs
@@ -4,8 +4,9 @@ use std::sync::RwLock;
 
 use once_cell::sync::Lazy;
 
-pub static DOCDIR: Lazy<RwLock<String>> = Lazy::new(|| RwLock::new(String::from("./")));
+pub static DOCDIR: Lazy<RwLock<String>> = Lazy::new(|| RwLock::new(String::from("./data")));
 
 pub async fn init_db() -> Result<()> {
+    std::fs::create_dir_all(&*DOCDIR.read().unwrap())?;
     Ok(())
 }
